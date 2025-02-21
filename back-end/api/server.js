4d648e39fa9c0e6268143a1c887bc9203ef4c57c
Felix♨ <felipedasilvapereira@outlook.com>
2025-02-20 21:11:42 -0300

    ajustes para o deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -7,6 +7,9 @@
 import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
+import path from "path";
+
+const __direname = path.resolve();
 
 const app = express();
 const PORT = 3000;
@@ -14,18 +17,24 @@ const PORT = 3000;
 app.use(cors());
 // app.use(express.json());
 
-app.get("/", (request, response) => {
+app.get("/api/", (request, response) => {
   response.send("Só vamos trabalhar com os endpoints'/artists' e 'songs'");
 });
 
-app.get("/artists", async (request, response) => {
-  console.log("Requisição para /artists recebida");
+app.get("/api/artists", async (request, response) => {
+  console.log("Requisição para /artists recebida"); // Requisição para saber se artitsts ta funcionando
   response.send(await db.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", async (request, response) => {
-  console.log("Requisição para /songs recebida");
+app.get("/api/songs", async (request, response) => {
+  console.log("Requisição para /songs recebida"); // Requisição para saber se songs ta funcionando
   response.send(await db.collection("songs").find({}).toArray());
+});
+
+app.use(express.static(path.join(__direname, "../front-end/dist")));
+
+app.get("*", async (request, response) => {
+  response.sendFile(path.join(__direname, "../front-end/dist/index.html"));
 });
 
 app.listen(PORT, () => {
